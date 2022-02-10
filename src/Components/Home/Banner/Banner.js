f02aefe71d0cc69f6fd1ea9a92589c834cb2df3b
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-10 10:56:47 +0600

    banner img added

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
-            <h1>this is banner.</h1>
+        <div className='BannerBg pt-40 pb-64'>
+            <div className='text-center'>
+                <h1 className="text-4xl font-semibold pb-5">Best food waiting for your belly</h1>
+                <input type="search" name="" id="" placeholder='Search food item' />
+                <button>Search</button>
+            </div>
         </div>
     );
 };
